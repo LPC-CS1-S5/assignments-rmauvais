aafc4c00d5222956448c1e300359128faf0347c7
rmauvais <rpmauvais@zonemail.clpccd.edu>
2021-03-15 05:21:10 +0000

    q

---
+++ Chapter 4/assignment4-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() 
+{
+  int num1, num2, num3, max, min;
+    printf("Enter three numbers: ");
+    scanf("%d%d%d", &num1, &num2, &num3);
+
+
+    if((num1 > num2) && (num1 > num3))
+    {
+        max = num1;
+    }
+    else if(num2 > num3)
+    {
+        max = num2;
+    }
+    else
+    {
+        max = num3;
+    }
+
+    if((num1 < num2) && (num1 < num3))
+    {
+        min = num1;
+    }
+    else if(num2 < num3)
+    {
+        min = num2;
+    }
+    else
+    {
+        min = num3;
+    }
+    printf("Maximum among all three numbers = %d", max);
+    printf("Minimum among all three numbers = %d", min);
+    return 0;
+}
