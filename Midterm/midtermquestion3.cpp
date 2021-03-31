32ebd7ae04fc0773e7c32c909cc47372be100321
rmauvais <rpmauvais@zonemail.clpccd.edu>
2021-03-31 16:14:05 +0000

    x

--- Midterm/midtermquestion3.cpp
+++ Midterm/midtermquestion3.cpp
@@ -10,5 +10,32 @@ using namespace std;
 int getRdnum()
 
 {
+  return 1 + rand() % 50;
+}
+
+int isGreater(int n)
+
+{
+  for (int i=2); i < n / 2; i++)
+  {
+    if (n % i == 0)
+    return 0;
+  }
+  return 1;
+}
+
+int main()
+
+{
+  srand(time(0));
+  
+  ofstream output("numbers.txt");
+
+  if (!output.is_open())
+  {
+    cout << "Error opening file!!\n";
+
+    return -1;
+  }
   
 }
