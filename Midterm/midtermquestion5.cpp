d537f85cbe8fe07c697e809f448c7ee7ba83d234
rmauvais <rpmauvais@zonemail.clpccd.edu>
2021-03-31 16:53:21 +0000

    x

---
+++ Midterm/midtermquestion5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+int main() 
+{
+  int firstNumber, secondNumber;
+
+  cout << "Enter two integers: ";
+  cin >> firstNumber >> secondNumber;
+
+  int division = firstNumber / secondNumber;
+  cout<< division;
+{
+  ofstream output(question5.txt);
+
+  if (!output.isopen())
+  {
+    cout << "Error opening file \n";
+
+  }
+}
+  return -1;
+}
