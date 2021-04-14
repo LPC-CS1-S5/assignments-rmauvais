3c640810a630035b4d9c325be5dbd6ce4ade53c9
rmauvais <>
2021-04-14 04:12:45 +0000

    a

---
+++ Chapter 6/quiz6-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int &x, int & y)
+
+{
+  int t = x; x-y; y=t;
+  cout <<"\n Within Swapping function x = "<<x<<" and y "<<y;
+}
+
+int main ()
+{
+  int x,y;
+  
+}
