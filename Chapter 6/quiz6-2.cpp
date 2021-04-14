6ca1e7e507267fe42c0b9aaf63a8b18f32a5bb28
rmauvais <>
2021-04-14 05:38:32 +0000

    b

--- Chapter 6/quiz6-2.cpp
+++ Chapter 6/quiz6-2.cpp
@@ -6,7 +6,7 @@ void getRandnum(int &n1, int &n2, int &n3);
 
 int main()
 {
-  int n1, n2, n3;
+  int n1, n2, n3, min;
 int findMin(int n1, int n2, int n3);
 getRandnum(n1, n2, n3);
 min = findMin(n1, n2, n3);
