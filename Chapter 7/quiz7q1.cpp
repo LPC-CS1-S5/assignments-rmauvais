6444711768947fc2b9414968e05e65940898f261
rmauvais <>
2021-05-05 04:12:51 +0000

    d

--- Chapter 7/quiz7q1.cpp
+++ Chapter 7/quiz7q1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 const int N = 10;
@@ -8,5 +9,7 @@ if(index==0&&(abs(a[index]-a[index+1])<10))
 return true;
 if(index==N-1&&(abs(a[index]-a[index-1])<10))
 return true;
-if(index!=N && index!=0&&)
+if(index!=N && index!=0&&(abs(a[index]-a)[index+1]<10))&&(abs(a[index]-a[index-1])<10))
+return true;
+return false;
 }
