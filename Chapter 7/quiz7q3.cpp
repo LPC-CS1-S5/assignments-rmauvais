e798c179d445cf4aec3db79ee18451f05f4ca471
rmauvais <>
2021-05-05 05:11:09 +0000

    t

--- Chapter 7/quiz7q3.cpp
+++ Chapter 7/quiz7q3.cpp
@@ -8,5 +8,8 @@ int numbers[50], numbers1[20], numbers2[20], max1, max2, n, i, a = 0;
 cout<<"Enter the value of n: "; //takes an input of how many numbers
 cin>>n;
 cout<<"Enter the numbers: "<<endl;
-for(i= 0;)
+for(i= 0;i<n ; i++)
+{
+  cin>>numbers[i];
+}
 }
