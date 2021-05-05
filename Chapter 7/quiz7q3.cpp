ff70950d5c230393623cbb7f079956bc47bff1ce
rmauvais <>
2021-05-05 05:04:56 +0000

    g

--- Chapter 7/quiz7q3.cpp
+++ Chapter 7/quiz7q3.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int findmax(int numbers[], int, int);
 int main()
 {
-
+int numbers[50], numbers1[20], numbers2[20], max1, max2, n, i, a = 0;
+cout<<"Enter the value of n: "; //takes an input of how many numbers
+cin>>n;
+cout<<"Enter the numbers: "<<endl;
+for(i= 0;)
 }
