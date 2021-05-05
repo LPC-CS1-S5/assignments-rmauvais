a3aef3c5ed62f032295ec765ac41200b88dddbb7
rmauvais <>
2021-05-05 05:17:12 +0000

    o

--- Chapter 7/quiz7q3.cpp
+++ Chapter 7/quiz7q3.cpp
@@ -15,4 +15,21 @@ for(i= 0;i<n ; i++)
 cout<<"The given numbers is..."<<endl;
 cout<<" [ ";
 for(i=0; i<n; i++) // the numbers divide into two slots to be stored in those designated slots.
+{
+  if(i < n/2) // numbers stored in the first slot
+  {
+    numbers1[i] = numbers[i];
+    cout<<numbers1[i]<<" , ";
+  }
+}
+  else // numbers in the second slot
+{
+  numbers2[i] = numbers[i];
+  cout<<numbers2[i]<<" , ";
+}
+{
+  cout<<"] "<<endl;
+  max1 = findmax(numbers1, a, n/2); // function calling for the max number in slot 1
+  cout<<"Max number from slot 1 : "<<max1<<"\t"; // print the max number in slot 1
+}
 }
