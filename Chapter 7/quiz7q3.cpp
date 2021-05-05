44c1c677164fdeafcbb526ddcf40532c03ae615c
rmauvais <>
2021-05-05 05:19:12 +0000

    b

--- Chapter 7/quiz7q3.cpp
+++ Chapter 7/quiz7q3.cpp
@@ -30,6 +30,13 @@ for(i=0; i<n; i++) // the numbers divide into two slots to be stored in those de
 {
   cout<<"] "<<endl;
   max1 = findmax(numbers1, a, n/2); // function calling for the max number in slot 1
-  cout<<"Max number from slot 1 : "<<max1<<"\t"; // print the max number in slot 1
+  cout<<"Max number from slot 1 : "<<max1<<"\t"; // print the max number of slot 1
+  max2 = findmax(numbers2, n/2, n); // function calling for the max number in slot 2
+  cout<<"Max number from slot 2 : "<<max2; // print max number of slot 2
+  return 0;
+}
+int findmax(int numbers[], int from, int to)// function definition for finding the max numbers
+{
+  int i, t = numbers [0]
 }
 }
