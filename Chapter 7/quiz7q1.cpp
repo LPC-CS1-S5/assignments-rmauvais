b80fc09fd9a17c37db7d20bd08b712d2af05fab4
rmauvais <>
2021-05-05 04:18:16 +0000

    c

--- Chapter 7/quiz7q1.cpp
+++ Chapter 7/quiz7q1.cpp
@@ -5,11 +5,16 @@ using namespace std;
 const int N = 10;
 bool check(int a[],int index)
 {
+//checks if index is 0 and compares with the next element
 if(index==0&&(abs(a[index]-a[index+1])<10))
 return true;
+//checks if index is N-1 then checks with the previous element
 if(index==N-1&&(abs(a[index]-a[index-1])<10))
 return true;
 if(index!=N && index!=0&&(abs(a[index]-a)[index+1]<10))&&(abs(a[index]-a[index-1])<10))
 return true;
 return false;
+}
+int main(){
+  int number[N]={10,15,0,-25,19,20,25,30,45,20};
 }
