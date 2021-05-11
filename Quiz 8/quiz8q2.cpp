05c4ff921fdbc751323fcb6470619ec12a6be7b8
rmauvais <>
2021-05-11 06:31:18 +0000

    b

--- Quiz 8/quiz8q2.cpp
+++ Quiz 8/quiz8q2.cpp
@@ -1,4 +1,23 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+int main()
+{
+  while(true)
+  {
+    string email;
+    int at_pos=-1, dot_pos=-1,i;
+    cout<<"Enter emailId:";
+    getline(cin,email);
+    if(email=="q")
+    {
+      break;
+    }
+    else
+    {
+      if(email[0]>='0'&& email[0]<='9')
+    }
+  }
+}
