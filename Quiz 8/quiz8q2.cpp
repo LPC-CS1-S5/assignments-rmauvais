d024a9da281ccacf18f99945873e1994d545f0fe
rmauvais <>
2021-05-11 06:34:53 +0000

    e

--- Quiz 8/quiz8q2.cpp
+++ Quiz 8/quiz8q2.cpp
@@ -18,6 +18,28 @@ int main()
     else
     {
       if(email[0]>='0'&& email[0]<='9')
+      {
+        cout<<"The Email ID"<<email<<" is invalid"<<endl;
+        cout<<"* Email Id should not start with a number"<<endl;
+      }
+      else
+      {
+        for(i=0 ;i<email.length();i++)
+        {
+          if(email[i]=='@')
+          {
+            at_pos=i;
+          }
+          if(email[i]=='.')
+          {
+            dot_pos=i;
+          }
+        }
+        if (at_pos==-1)
+        {
+          
+        }
+      }
     }
   }
 }
