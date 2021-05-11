f5a546d74ef5ad03b60a4528c3b68462602be2d1
rmauvais <>
2021-05-11 05:57:03 +0000

    r

---
+++ Quiz 8/quiz8q1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int countword(string, string);
+int main(int argc, char const *argv[])
+
+{
+  int cnt;
+
+  string txt("The Python 2 language was officially discontinued in 2020 (first planned for 2015), and Python 2.7.18 is the last Python 2.7 release and therefore the last Python 2 release. No more security patches or other improvements will be released for it. With Python 2's end-of-life, only Python 3.5.x and later are supported. thither")
+}
