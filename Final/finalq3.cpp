7450255c7e259cefc95bb916236685b2eae073f1
rmauvais <>
2021-05-25 22:05:27 +0000

    q

--- Final/finalq3.cpp
+++ Final/finalq3.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main()
 {
   const int SIZE1 = 5;
-  const int SIZE 2 = 10;
+  const int SIZE2 = 10;
+
+  int array1[SIZE1] = { 2, 3, 5, 9, 10 };
+  int array2[SIZE2] = { 1, 2, 3, 5, 9, 10, 11, 13, 15, 17 };
+
   
 }
