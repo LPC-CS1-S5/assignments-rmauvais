16e76b46fc8202712df0ff1c7577f3fa4023ef8f
rmauvais <>
2021-05-25 22:11:49 +0000

    s

--- Final/finalq3.cpp
+++ Final/finalq3.cpp
@@ -7,21 +7,21 @@ int main()
   const int SIZE1 = 5;
   const int SIZE2 = 10;
 
-  int array1[SIZE1] = { 2, 3, 5, 9, 10 };
-  int array2[SIZE2] = { 1, 2, 3, 5, 9, 10, 11, 13, 15, 17 };
+  int array1[SIZE1] = { 2, 3, 5, 9, 10 }; //array 1
+  int array2[SIZE2] = { 1, 2, 3, 5, 9, 10, 11, 13, 15, 17 }; // array 2
 
   for (size_t i = 0; i < SIZE2; i++)
   {
-    bool in_common = false;
+    bool in_common = false; //False
 
     for (size_t j = 0; j < SIZE2; j++)
     {
       if (array1[i] == array2[j])
       {
-        in_common = true;
+        in_common = true; //True
       }
     }
-    if (in_common)
+    if (in_common) // if true print the common 
     {
       std:: cout << array1[i] << " in common in both arrays\n";
     }
