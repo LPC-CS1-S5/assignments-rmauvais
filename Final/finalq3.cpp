eb577f04963a2fcf64c3811ef862ea2db4649dc0
rmauvais <>
2021-05-25 22:08:40 +0000

    L

--- Final/finalq3.cpp
+++ Final/finalq3.cpp
@@ -10,5 +10,20 @@ int main()
   int array1[SIZE1] = { 2, 3, 5, 9, 10 };
   int array2[SIZE2] = { 1, 2, 3, 5, 9, 10, 11, 13, 15, 17 };
 
-  
+  for (size_t i = 0; i < SIZE2; i++)
+  {
+    bool in_common = false;
+
+    for (size_t j = 0; j < SIZE2; j++)
+    {
+      if (array1[i] == array2[j])
+      {
+        in_common = true;
+      }
+    }
+    if (in_common)
+    {
+      std:: cout << array1[i] << " in common in both arrays\n";
+    }
+  }
 }
