7978534c2e919988df74a9719b193b0a5465ba39
rmauvais <>
2021-05-26 17:58:24 +0000

    s

--- Final/finalq5.cpp
+++ Final/finalq5.cpp
@@ -26,6 +26,18 @@ int main()
 
   ifs.open("grades.bin");
   if(! ifs){
-    cout <<"File Open Error"
+    cout <<"File Open Error : Sure that there is the file grades.bin \n";
+    exit(0);
+  }
+  while ( ifs.read((char *)&g, sizeof(g))){
+    printstruct(g);
   }
 }
+void printstruct(Grade g)
+{
+  cout << "Name : " << g.first << " " << g.last << "\t" << " SSN: " << g.ssn << "\n";
+  cout << "\tScores: " ;
+  for(int i=0; i<N;i++)
+    cout << showpoint << setprecision(2) << g.score.sc[i] << "\t";
+    cout << "Grade " << g.score.grade << endl;
+ }
